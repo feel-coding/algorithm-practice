0e9ed0fb5c0a0f919a50955c695ee7ec447acf11
feel-coding <khe628@naver.com>
2020-10-28 19:51:18 +0900

    백준 11726

---
+++ 백준 11726.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+	arr[0] = 1;
+	arr[1] = 2;
+	for (int i = 2; i < n; i++) {
+		arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+	}
+	cout << arr[n - 1];
+	return 0;
+}
