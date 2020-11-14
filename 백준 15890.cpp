4dbe65a8a4a23304f9e254ba483323de272ef186
feel-coding <khe628@naver.com>
2020-11-15 01:37:26 +0900

    백준 15890

---
+++ 백준 15890.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cout << "20180729";
+	return 0;
+}
