d478342ce558a575912adb8fcff6f05a29a4bd29
feel-coding <khe628@naver.com>
2020-12-02 20:48:18 +0900

    백준 9316

---
+++ 백준 9316.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cout << "Hello World, Judge " << i << "!\n";
+	}
+	return 0;
+}
