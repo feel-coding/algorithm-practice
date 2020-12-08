f25357f44930bb9842bff4f1c4094b3a7f9e14d7
feel-coding <khe628@naver.com>
2020-12-08 20:38:35 +0900

    백준 11382

---
+++ 백준 11382.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	long a, b, c;
+	cin >> a >> b >> c;
+	cout << a + b + c;
+	return 0;
+}
