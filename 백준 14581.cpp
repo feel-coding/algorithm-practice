24be20301c1edbba7a17ef5baed7d65be443609b
feel-coding <khe628@naver.com>
2020-12-17 22:11:05 +0900

    백준 14581

---
+++ 백준 14581.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	string s;
+	cin >> s;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << s << "::fan:\n";
+	cout << ":fan::fan::fan:\n";
+	return 0;
+}
