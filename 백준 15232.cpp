409e13bb972bcd39f08dd7911a3f26bcc1782d03
feel-coding <khe628@naver.com>
2020-12-21 23:29:22 +0900

    백준 15232

---
+++ 백준 15232.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int r, c;
+	cin >> r >> c;
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < c; j++) {
+			cout << "*";
+		}
+		cout << '\n';
+	}
+	return 0;
+}
