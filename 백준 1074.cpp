3ea0473f262c53056fa09d0272fc6f6370cb3ffa
feel-coding <khe628@naver.com>
2021-01-04 16:12:00 +0900

    재채점으로 시간초과되어 다시 품

--- 백준 1074.cpp
+++ 백준 1074.cpp
@@ -3,54 +3,34 @@
 #include <cmath>
 using namespace std;
 
-int n, r, c;
-int cnt = 0;
-int result;
-bool found = false;
-
-void visit(int rowStart, int rowEnd, int colStart, int colEnd) {
-	if (found)
-		return;
-	if (rowEnd - rowStart == 1) {
-		if (++cnt && rowStart == r && colStart == c) {
-			//cnt++;
-			result = cnt;
-			found = true;
-			return;
-		}
-		else if (++cnt && rowStart == r && colEnd == c) {
-			//cnt++;
-			result = cnt;
-			found = true;
-			return;
-		}
-		else if (++cnt && rowEnd == r && colStart == c) {
-			//cnt++;
-			result = cnt;
-			found = true;
-			return;
-		}
-		else if (++cnt && rowEnd == r && colEnd == c) {
-			//cnt++;
-			result = cnt;
-			found = true;
-			return;
-		}
-		else {
-			//cnt += 4;
-			return;
-		}
-	}
-	visit(rowStart, rowStart + (rowEnd + 1 - rowStart) / 2 - 1, colStart, colStart + (colEnd + 1 - colStart) / 2 - 1);
-	visit(rowStart, rowStart + (rowEnd + 1 - rowStart) / 2 - 1, colStart + (colEnd + 1 - colStart) / 2, colEnd);
-	visit(rowStart + (rowEnd + 1 - rowStart) / 2, rowEnd, colStart, colStart + (colEnd + 1 - colStart) / 2 - 1);
-	visit(rowStart + (rowEnd + 1 - rowStart) / 2, rowEnd, colStart + (colEnd + 1 - colStart) / 2, colEnd);
-}
-
 int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int n, r, c;
 	cin >> n >> r >> c;
 	n = pow(2, n);
-	visit(0, n - 1, 0, n - 1);
-	cout << result - 1;
+	int index = 0;
+	while (true) {
+		if (n == 1) {
+			break;
+		}
+		if (r < n / 2 && c < n / 2) {
+		}
+		else if (r < n / 2 && c >= n / 2) {
+			c -= n / 2;
+			index += pow(n / 2, 2);
+		}
+		else if (r >= n / 2 && c < n / 2) {
+			r -= n / 2;
+			index += pow(n / 2, 2) * 2;
+		}
+		else {
+			r -= n / 2;
+			c -= n / 2;
+			index += pow(n / 2, 2) * 3;
+		}
+		n /= 2;
+	}
+	cout << index;
 	return 0;
 }
