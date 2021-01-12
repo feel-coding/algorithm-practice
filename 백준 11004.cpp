4a65b54ffe638d447cbbd361bcde519f6903f065
feel-coding <khe628@naver.com>
2021-01-12 18:48:35 +0900

    백준 11004

---
+++ 백준 11004.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int n, k;
+	cin >> n >> k;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+	cout << v[k - 1];
+	return 0;
+}
