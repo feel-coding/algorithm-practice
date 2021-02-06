5abeba0796933715c1f927dd95297a07ccffadbf
feel-coding <khe628@naver.com>
2021-02-06 23:49:18 +0900

    백준 13985

---
+++ 백준 13985.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin, s);
+	if (s[0] - '0' + s[4] - '0' == s[8] - '0') {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+	return 0;
+}
