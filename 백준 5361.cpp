2c8f59b4ca72b1732e87a1bfc23d2023cf2881f5
feel-coding <khe628@naver.com>
2021-02-17 23:14:50 +0900

    백준 5361

---
+++ 백준 5361.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int testCase;
+	cin >> testCase;
+	int a, b, c, d, e;
+	cout.precision(2);
+	for (int t = 0; t < testCase; t++) {
+		cin >> a >> b >> c >> d >> e;
+		cout << "$" << fixed << a * 350.34 + b * 230.90 + c * 190.55 + d * 125.30 + e * 180.90 << '\n';
+	}
+	return 0;
+}
