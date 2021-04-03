14ef4fef31a1c1ef24b5d56ac17eb1c70255fa41
feel-coding <khe628@naver.com>
2021-04-03 13:37:04 +0900

    디퓨저 - B

---
+++ 디퓨저 - B.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int arr[105][105];
+bool visited[105][105];
+int dr[4] = { -1, 1, 0, 0 };
+int dc[4] = { 0, 0, -1, 1 };
+
+struct location {
+	int row;
+	int col;
+	int distance;
+};
+
+int main() {
+	int n, m, row, col;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+			if (arr[i][j] == 2) {
+				row = i;
+				col = j;
+			}
+		}
+	}
+	int maxDistance = 0;
+	queue<location> q;
+	q.push({ row, col, 0 });
+	while (!q.empty()) {
+		location cur = q.front();
+		q.pop();
+		if (!visited[cur.row][cur.col]) {
+			if (cur.distance > maxDistance) maxDistance = cur.distance;
+			visited[cur.row][cur.col] = true;
+			for (int i = 0; i < 4; i++) {
+				int nr = cur.row + dr[i];
+				int nc = cur.col + dc[i];
+				if (nr < 0 || nc < 0 || nr >= n || nc >= m) continue;
+				if (arr[nr][nc] == 0) q.push({ nr, nc, cur.distance + 1 });
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (arr[i][j] == 0 && !visited[i][j]) {
+				cout << -1;
+				return 0;
+			}
+		}
+	}
+	cout << maxDistance;
+	return 0;
+}
