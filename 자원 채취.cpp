dbd2856b08ee658c437f9d8af0fc4df56a9c98f9
feel-coding <khe628@naver.com>
2021-05-04 23:49:06 +0900

    자원 채취

---
+++ 자원 채취.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1005][1005];
+int dp[1005][1005];
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			dp[i][j] = arr[i][j] + max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+	cout << dp[n][m];
+	return 0;
+}
