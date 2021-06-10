93e656c3519ca0a10f8dad26922de93899a45294
feel-coding <khe628@naver.com>
2021-06-10 22:56:47 +0900

    밀기 알고리즘 2

---
+++ 밀기 알고리즘 2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int arr[105][105];
+
+int main() {
+	int n, m, u, d;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+		}
+	}
+	cin >> u >> d;
+	int temp = arr[0][m - 1];
+	for (int i = m - 1; i >= 1; i--) {
+		arr[0][i] = arr[0][i - 1];
+	}
+	for (int i = 0; i < u; i++) {
+		arr[i][0] = arr[i + 1][0];
+	}
+	for (int i = 0; i < m - 1; i++) {
+		arr[u][i] = arr[u][i + 1];
+	}
+	for (int i = u; i > 0; i--) {
+		arr[i][m - 1] = arr[i - 1][m - 1];
+	}
+	arr[1][m - 1] = temp;
+	temp = arr[d][0];
+	for (int i = 0; i < m - 1; i++) {
+		arr[d][i] = arr[d][i + 1];
+	}
+	for (int i = d; i < n - 1; i++) {
+		arr[i][m - 1] = arr[i + 1][m - 1];
+	}
+	for (int i = m - 1; i >= 1; i--) {
+		arr[n - 1][i] = arr[n - 1][i - 1];
+	}
+	for (int i = n - 1; i > d; i--) {
+		arr[i][0] = arr[i - 1][0];
+	}
+	arr[d + 1][0] = temp;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << '\n';
+	}
+	return 0;
+}
